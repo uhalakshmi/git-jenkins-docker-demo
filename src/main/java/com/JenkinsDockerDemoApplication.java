6709139697895d53054d75e772c51770910c30e2
uhalakshmi <uhalakshmit@gmail.com>
2021-09-09 14:27:14 +0530

    my commit

---
+++ src/main/java/com/JenkinsDockerDemoApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDockerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDockerDemoApplication.class, args);
+	}
+
+}
